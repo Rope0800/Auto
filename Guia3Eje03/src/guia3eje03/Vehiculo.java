598259f9793fcb7dd92a6f7c7160559686e75a8d
whatdafaqq <ramiromoran667@gmail.com>
2023-08-17 00:41:42 -0300

    Interface Vehiculo
    Creacion de la interface vehiculo.

---
+++ Guia3Eje03/src/guia3eje03/Vehiculo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package guia3eje03;
+
+/**
+ *
+ * @author ramir
+ */
+public interface Vehiculo {
+    
+}
